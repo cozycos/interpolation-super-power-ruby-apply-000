58fcc6306ffd866a30699d937378e2c09a8cb21e
resourceful-composition-1273 <cozycosmic@gmail.com>
2020-05-23 14:17:24 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,3 @@
-# Write your #display_rainbow method here
+display_rainbow (['red','orange','yellow','green','blue','indigo','violet']).
+
+"R:Red, O:Orange, Y:yellow, G:green, B:blue, I:indigo, V:violet"
